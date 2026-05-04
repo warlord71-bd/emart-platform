e85e7c09137a8956320e20bdd995e97f4c209382
Warlord <support@e-mart.com.bd>
2026-05-04 13:07:47 +0200

    chore: remove about-us page, redirect to our-story

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -81,6 +81,8 @@ const nextConfig = {
       { source: '/homepage/',              destination: '/',                 permanent: true },
       { source: '/shop/%e0%a6%a6%e0%a6%be%e0%a6%97-%e0%a6%aa%e0%a6%bf%e0%a6%97%e0%a6%ae%e0%a7%87%e0%a6%a8%e0%a7%8d%e0%a6%9f%e0%a7%87%e0%a6%b6%e0%a6%a8-%e0%a6%93-%e0%a6%a1%e0%a6%be%e0%a6%b0%e0%a7%8d%e0%a6%95-%e0%a6%b8', destination: '/shop/beaute-glutathione-skin1004-centella-brightening-combo-2pcs', permanent: true },
       { source: '/category/night-crime',   destination: '/category/night-cream', permanent: true },
+      { source: '/about-us',              destination: '/our-story',      permanent: true },
+      { source: '/about-us/',             destination: '/our-story',      permanent: true },
     ];
   },
 
